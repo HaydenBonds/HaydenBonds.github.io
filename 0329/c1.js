b4d86366b34f4457e3eb8b6e60bc414112bd62af
HaydenBonds <Hayden.D.Bonds-1@ou.edu>
2024-03-29 10:22:12 -0500

    LC0329

--- 0329/c1.js
+++ 0329/c1.js
@@ -1,47 +1,47 @@
 "use strict"
 
-let b1 = document.querySelector("#b1");
+let b2 = document.querySelector("#b2");
 let R = null;
-let chart1 = null;
-async function loadd()
+let chart2 = null;
+
+async function showc()
 {
-    let url = `https://juxinglong.github.io/static/data/states.json`
-    let r=await fetch(url);
+    let url = `https://juxinglong.github.io/static/HW/hw6/MIS3033.json`;
+    let r = await fetch(url);
     let rj = await r.json();
 
-    let c1 = document.querySelector("#c1");
-    if (chart1 != null)
-    {
-        chart1.destroy();
-
-    }
-    c1.innerHTML = ``;
+    let c2 = document.querySelector("#c2");
 
     let opts = {
         type: "pie",
-        
         data: {
-            labels: rj.map(x => x.st),
-            datasets: [
-                {data:rj.map(x=>x.p),},
-            ],
+            labels: rj.map(x=>x.lg),
+            datasets: [{data: rj.map(x=>x.n), label:"Number" },],
         },
+    };
+
+    if (chart2 != null)
+    {
+        chart2.destroy();
     }
+    c2.innerHTML = ``;
 
-    chart1 = new Chart(c1,opts);
+    chart2 = new Chart(c2, opts);
 
+    //anime.js
+    opts={
+        targets: [c2,],
+            rotate:{ value: 60, duration: 3000,}
+
+    };
+    anime(opts);
+ 
+
+
+
+    R = rj;
+    console.log(R);
     
-
-
-
-
-
-
-    R = rj
-    console.log(rj);
-
-    Swal.fire("Load Data")
-
 }
 
-b1.addEventListener("click",loadd)
+b2.addEventListener("click",showc)
